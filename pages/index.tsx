39c4efe15b4b39d1c1fda62eabd697196286dbbb
Josh Kellendonk <josh@colloco.com>
2020-06-12 14:48:35 -0600

    Change to typescript

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout'
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Propositional Logic Solutions</h1>
+    <p>
+      This site is a placeholder for what will eventually become the solutions for Howard Pospesel's Propositional Logic book.
+    </p>
+  </Layout>
+)
+
+export default IndexPage
