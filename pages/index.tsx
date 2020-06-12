c967d074734fdf29da671d7d4c80372b4afc0be1
Josh Kellendonk <josh@colloco.com>
2020-06-12 14:54:18 -0600

    Add permission note.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Propositional Logic Solutions</h1>
     <p>
-      This site is a placeholder for what will eventually become the solutions for Howard Pospesel's Propositional Logic book.
+      This site is a placeholder for what will eventually become the solutions for Howard Pospesel's Propositional Logic book. I am currently waiting for permission to publish the exercises.
     </p>
   </Layout>
 )
